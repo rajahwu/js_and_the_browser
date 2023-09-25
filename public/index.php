f17726bd7ca01c438112a10d12ab3f902dc36e01
rajahwu <vmradford@gmail.com>
2023-09-25 12:53:30 -0400

    add js to create slideshow divs

--- public/index.php
+++ public/index.php
@@ -8,10 +8,14 @@ $Parsedown = new Parsedown();
 <div class="container flex">
     <div class="prose">
         <h1>The Browser</h1>
+        <div id="slideshow-container"></div>
         <?php echo $Parsedown->text(file_get_contents("../readme.md")); ?>
     </div>
-    <div id="react_root" style="display: none;"></div>
 </div>
+<div id="react_root" style="display: none;"></div>
+</div>
+
+<script src="./js/slideshow.js"></script>
 <script src="../node_modules/react/umd/react.production.min.js"></script>
 <script src="../node_modules/react-dom/umd/react-dom.production.min.js"></script>
 <script src="./dist/bundle.js"></script>
--- readme.md
+++ readme.md
@@ -2,8 +2,16 @@
 
 ## [Networks and the Internet](../pages/networks_and_internet/index.php)
 
+![drawing of server](assets/images/networks_title.png)
+
 ## [The Document Object Model](../pages/the_dom/index.php)
+
+![drawing of tree](assets/images/the_dom_title.png)
 
 ## [Handling Events](../pages/events/index.php)
 
+![drawing of tools](assets/images/events_title.png)
+
 ## [Project: A Platform Game](../pages/game/index.php)
+
+![drawing of a platform game](assets/images/game_title.png)
