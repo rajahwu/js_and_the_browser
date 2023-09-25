305abef175a098dbf8f983ace6c3a2fb0606f072
rajahwu <vmradford@gmail.com>
2023-09-24 22:58:31 -0400

    add code editor

--- public/js/react/HomePage.jsx
+++ public/js/react/HomePage.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai"; // Choose a theme
+
 
 export default function HomePage() {
-    return <h1>Hello from React</h1>;
+    const [value, setValue] = React.useState("console.log('hello world!');");
+    const onChange = React.useCallback((val, viewUpdate) => {
+        console.log('val:', val);
+        setValue(val);
+      }, []);
+    return <AceEditor
+        mode="javascript" // Set the programming language mode
+        theme="monokai"   // Choose a theme
+        name="code-editor"
+        editorProps={{ $blockScrolling: true }}
+        value={value} // Set the initial code value
+        onChange={onChange} // Handle code changes
+        fontSize={14} // Set font size
+        width="100%" // Set the editor width
+        height="250px" // Set the editor height
+    />
+
 }
